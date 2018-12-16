0c9f4a168a52b33b4fe36b0fae5fa80c60700b1e
Walter Antolini <antwal@gmail.com>
2018-12-17 00:19:56 +0100

    Renamed unit test script

---
+++ unittest.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+chmod a+x ./submodules.sh
+./submodules.sh
+
+chmod a+x ./tests/run
+
+buildImages() {
+    for DIR in debian-stretch-{minimal,standard}; do
+        if [ -d "$DIR" ] && [ -f "$DIR/Dockefile" ]; then
+            echo "Start build $DIR"
+            export currentOwner="$(id -u -n):$(id -g -n)"
+            sudo owner=$currentOwner buildDir="$DIR" imageName="antwal/$DIR:$1" ./tests/run cache logger noclean
+        else
+            echo "No build $DIR"
+        fi
+    done
+}
+
+# Build all main images
+buildImages latest
+
+# Remove dangling images
+#docker rmi $(docker images -f "dangling=true" -q)

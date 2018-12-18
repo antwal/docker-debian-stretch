f96b783eb81ab32a63296b2fab21973fd8b7edd1
Walter Antolini <antwal@gmail.com>
2018-12-18 21:43:22 +0100

    Update README minimal with TODO Added Unit Test for makepasswd

---
+++ tests/files/debian-stretch-makepasswd.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##############################################################################
+## Tests
+##############################################################################
+
+function testCommandCheckEntrypoint() {
+    echo -e "${COLOR}function $containerName()${NC}" > "$redirect" 2>&1
+
+    docker run -i --nane "$containerName" "$imageName" | grep 'makepasswd v' \
+        > "$redirect" 2>&1
+    rtrn=$?
+    assertTrue "Command Check Entrypoint" ${rtrn}
+}
+
+function testCommandGeneratePassword() {
+    echo -e "${COLOR}function $containerName()${NC}" > "$redirect" 2>&1
+
+    echo -n "test-password" | docker run -i --nane "$containerName" \
+        "$imageName" --crypt-md5 --clearfrom=- | awk '{print $2}' \
+        > "$redirect" 2>&1
+    rtrn=$?
+    assertTrue "Command Generate Password" ${rtrn}
+}
+
+suite_addTest testCommandCheckEntrypoint
+suite_addTest testCommandGeneratePassword
